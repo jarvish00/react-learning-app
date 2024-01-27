361163314316c29ef85e9426ef2721115791338f
gyandeep <gyandeepsharma007@gmail.com>
2024-01-27 22:35:21 +0530

    code for side effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import React, { useEffect, useState } from 'react'
+
+import Login from './components/Login/Login'
+import Home from './components/Home/Home'
+import MainHeader from './components/MainHeader/MainHeader'
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    console.log('App component rendering in use effect ')
+    const userLoggedInInfo = localStorage.getItem('isLoggedIn')
+    if (userLoggedInInfo === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1')
+    setIsLoggedIn(true)
+  }
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
+    setIsLoggedIn(false)
+  }
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
