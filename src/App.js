3fd20b5e9c4ebbc96c8523577da088dde5217150
gyandeep <gyandeepsharma007@gmail.com>
2024-01-30 16:27:27 +0530

    code for hooks in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react'
+
 function App() {
+  console.log('App component start point ')
+  const [countState, setCountState] = useState({
+    count: 0,
+    name: 'gyandeep',
+  })
+
+  const incrementHandler = () => {
+    setCountState((prevState) => {
+      console.log('useState')
+      return { ...prevState, count: prevState.count + 1 }
+    })
+  }
+  useEffect(() => {
+    console.log('useEffect')
+  }, [])
+  console.log('App component start end ')
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    <>
+      <h2>{countState.name}</h2>
+      <h3>Count:{countState.count}</h3>
+      <button onClick={incrementHandler}>increment</button>
+    </>
+  )
 }
 
-export default App;
+export default App
